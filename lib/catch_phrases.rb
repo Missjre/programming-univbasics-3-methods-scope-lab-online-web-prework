c5c257f7238582c3539d3bb9a01b41359eda6b51
Jalysa Reason <jalysareason@gmail.com>
2019-08-13 23:42:23 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,8 +1,15 @@
 def mario
-  status = 'Thank You Mario! But Our Princess Is In Another Castle!'
-  puts phrase
+  puts out phrase
 end
 
 def toadstool
   puts status
 end
+
+def link 
+  puts out phrase
+end  
+
+def any_phrase
+  puts out phrase
+end  
